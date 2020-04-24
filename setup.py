1ccfbd42bb92c3ee740623c7b84696dd57ad9944
Primoz Marinsek <primoz.marinsek@ocado.com>
2020-04-24 15:38:03 +0100

    fixing

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author               = 'Primoz Marinsek',
     author_email         = 'primoz.marinsek@ocado.com',
     license              = "Apache-2.0",
-    packages             = find_packages('arubafi.*', 'arubafi'),
+    packages             = find_packages(),
     include_package_data = True,
     install_requires     = [
         'requests',
